a43ea0d478fce1b0e2834623c8f368d15ade36fc
Yoctopuce <dev@yoctopuce.com>
2019-06-04 18:12:23 +0200

    "New build based on revison 35635"

--- Sources/yocto_api.vb
+++ Sources/yocto_api.vb
@@ -780,7 +780,7 @@ Module yocto_api
 
   Public Const YOCTO_API_VERSION_STR As String = "1.10"
   Public Const YOCTO_API_VERSION_BCD As Integer = &H110
-  Public Const YOCTO_API_BUILD_NO As String = "35622"
+  Public Const YOCTO_API_BUILD_NO As String = "35635"
 
   Public Const YOCTO_DEFAULT_PORT As Integer = 4444
   Public Const YOCTO_VENDORID As Integer = &H24E0
